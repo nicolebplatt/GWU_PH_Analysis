ad4fe8d269633e21070d63130a464bd643200e48
nicolebplatt <nicoleplatt@Nicoles-MacBook-Air.local>
2022-04-05 14:53:37 -0500

    Update schema and add SQL query

--- schema.sql
+++ schema.sql
@@ -37,7 +37,7 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
@@ -48,4 +48,5 @@ CREATE TABLE dept_emp(
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no,dept_no)
 );
-SELECT * FROM departments;
+SELECT * FROM titles;
+DROP TABLE titles CASCADE;
